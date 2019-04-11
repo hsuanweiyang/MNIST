feb8559eab6f3cd9a3976460b54cc30e0c1036f1
Hsuan Wei Yang <e24006747@gmail.com>
2019-04-11 22:53:41 +0800

    model_cnn.py: add BN

--- model_cnn.py
+++ model_cnn.py
@@ -47,20 +47,25 @@ def initialize_parameters():
     return parameters
 
 
-def forward_propagation(X, parameters):
+def forward_propagation(X, parameters, is_training):
 
     W1 = parameters['W1']
     W2 = parameters['W2']
     W3 = parameters['W3']
 
     Z1 = tf.nn.conv2d(X, W1, strides=[1,1,1,1], padding='SAME')
-    A1 = tf.nn.relu(Z1)
+    A1 = tf.layers.batch_normalization(Z1, training=is_training)
+    A1 = tf.nn.relu(A1)
     Z2 = tf.nn.conv2d(A1, W2, strides=[1,1,1,1], padding='SAME')
-    A2 = tf.nn.relu(Z2)
+    A2 = tf.layers.batch_normalization(Z2, training=is_training)
+    A2 = tf.nn.relu(A2)
     P1 = tf.nn.max_pool(A2, ksize=[1,4,4,1], strides=[1,4,4,1], padding='SAME')
+
     Z3 = tf.nn.conv2d(P1, W3, strides=[1,1,1,1], padding='SAME')
-    A3 = tf.nn.relu(Z3)
+    A3 = tf.layers.batch_normalization(Z3, training=is_training)
+    A3 = tf.nn.relu(A3)
     P2 = tf.nn.max_pool(A3, ksize=[1,2,2,1], strides=[1,2,2,1], padding='SAME')
+
     P2 = tf.layers.flatten(P2)
     fc_layer_one = tf.keras.layers.Dense(30, activation=tf.nn.relu, use_bias=True)
     Z3 = fc_layer_one(P2)
@@ -109,23 +114,25 @@ def model_cnn(train_x, train_y, valid_x, valid_y, test_x, learning_rate=0.0009, 
     valid_costs = []
 
     X, Y = create_placeholders(n_h, n_w, n_c, n_y)
+    is_training = tf.placeholder(tf.bool)             # for batch normalization
     parameters = initialize_parameters()
 
 # train
-    Z3 = forward_propagation(X, parameters)
+    Z_out = forward_propagation(X, parameters, is_training)
     # compute cost
-    cost = compute_cost(Z3, Y)
+    cost = compute_cost(Z_out, Y)
     if regularization > 0.:
         regularize = 0
         for key in parameters.keys():
             regularize += tf.nn.l2_loss(parameters[key])
         cost = tf.reduce_mean(cost + regularization * regularize)
-    optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(cost)
+    with tf.control_dependencies(tf.get_collection(tf.GraphKeys.UPDATE_OPS)):
+        optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(cost)
     # evaluation
-    correct_prediction = tf.equal(tf.argmax(Z3, 1), tf.argmax(Y, 1))
+    correct_prediction = tf.equal(tf.argmax(Z_out, 1), tf.argmax(Y, 1))
     accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
 
-    predict_result = tf.argmax(Z3, 1)
+    predict_result = tf.argmax(Z_out, 1)
 
     init = tf.global_variables_initializer()
 
@@ -151,49 +158,51 @@ def model_cnn(train_x, train_y, valid_x, valid_y, test_x, learning_rate=0.0009, 
 
             for batch in range(num_minibatch):
                 (mini_x, mini_y) = minibatches[batch]
-                _, mini_cost = sess.run([optimizer, cost], {X:mini_x, Y:mini_y})
+                _, mini_cost = sess.run([optimizer, cost], {X: mini_x, Y: mini_y, is_training: True})
                 epoch_cost += mini_cost/num_minibatch
-                accuracy_train += accuracy.eval({X: mini_x, Y: mini_y}) / num_minibatch
+                accuracy_train += accuracy.eval({X: mini_x, Y: mini_y, is_training: False}) / num_minibatch
 
             if (epoch+1) % 10 == 0 and print_cost is True:
                 print('epoch_', epoch+1, ': ', epoch_cost)
             if (epoch+1) % 100 == 0:
                 print('[epoch-{0}]\n\tTrain Acc = {1}\tValid Acc = {2}'.format(epoch+1, accuracy_train,
-                                                                               accuracy.eval({X: valid_x, Y: valid_y})))
+                                                                               accuracy.eval({X: valid_x, Y: valid_y,
+                                                                                              is_training: False})))
             if (epoch+1) % 5 == 0:
                 costs.append(epoch_cost)
-                valid_costs.append(cost.eval({X:valid_x, Y: valid_y}))
+                valid_costs.append(cost.eval({X: valid_x, Y: valid_y, is_training: False}))
             if draw is True and (epoch+1) % 5 == 0:
                 plt.plot(costs, '-rx', label='Train')
                 plt.plot(valid_costs, '-bo', label='Test')
                 plt.pause(0.1)
 
-        # evaluation
+        # evaluation (cut train to small pieces preventing resource exhausted)
         train_batches_eval = random_mini_batches(train_x, train_y)
         num_train_batch_eval = len(train_batches_eval)
         accuracy_train = 0
         for train_batch in range(num_train_batch_eval):
             (mini_train_x, mini_train_y) = train_batches_eval[train_batch]
-            accuracy_train += accuracy.eval({X:mini_train_x, Y:mini_train_y})/num_train_batch_eval
+            accuracy_train += accuracy.eval({X: mini_train_x, Y: mini_train_y, is_training: False})/num_train_batch_eval
         print('Final Train Acc: ', accuracy_train)
-        print('Final Validation Acc:', accuracy.eval({X: valid_x, Y: valid_y}))
+        print('Final Validation Acc:', accuracy.eval({X: valid_x, Y: valid_y, is_training: False}))
 
         #output test
         out_result = []
         num_out_test = test_x.shape[0]
         batch_out_test = int(num_out_test/10)
         for i in range(9):
-            out_result.extend(predict_result.eval({X: test_x[i * batch_out_test: (i+1) * batch_out_test]}))
-        out_result.extend(predict_result.eval({X: test_x[9 * batch_out_test:]}))
+            out_result.extend(predict_result.eval({X: test_x[i * batch_out_test: (i+1) * batch_out_test],
+                                                   is_training: False}))
+        out_result.extend(predict_result.eval({X: test_x[9 * batch_out_test:], is_training: False}))
 
-        with open('out_test.csv', 'w') as test_out:
+        with open('submit_result.csv', 'w') as test_out:
             test_out.write('ImageId,Label\n')
             for i in range(len(out_result)):
                 test_out.write('{0},{1}\n'.format(i+1, out_result[i]))
         parameters = sess.run(parameters)
     return parameters
 
-
+'''
 def predict(X, parameters):
     x = tf.placeholder(tf.float32, [None, 28, 28, 1])
     Z3 = forward_propagation(x, parameters)
@@ -203,6 +212,7 @@ def predict(X, parameters):
         sess.run(init)
         result = sess.run(prediction, {x: X})
     return result
+'''
 
 
 if __name__ == '__main__':
