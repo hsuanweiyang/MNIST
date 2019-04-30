72c8531767d0b6f3337726c3a7a2e8eb6db9a49b
Hsuan Wei Yang <e24006747@gmail.com>
2019-04-30 15:42:58 +0800

    add save and restore model function

--- model_cnn.py
+++ model_cnn.py
@@ -2,10 +2,8 @@ import tensorflow as tf
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-import pickle
 from sys import argv
 import datetime
-from tensorflow.contrib.layers import fully_connected
 
 
 # modified Data
@@ -33,7 +31,7 @@ def train_valid(input_data, train_portion=0.97):
 
 def create_placeholders(n_h, n_w, n_c, n_y):
 
-    X = tf.placeholder(tf.float32, [None, n_h, n_w, n_c])
+    X = tf.placeholder(tf.float32, [None, n_h, n_w, n_c], name='input')
     Y = tf.placeholder(tf.float32, [None, n_y])
     return X, Y
 
@@ -43,11 +41,11 @@ def initialize_parameters():
     W1 = tf.get_variable('W1', [8, 8, 1, 16], initializer=tf.keras.initializers.he_normal())
     W2 = tf.get_variable('W2', [6, 6, 16, 32], initializer=tf.keras.initializers.he_normal())
     W3 = tf.get_variable('W3', [4, 4, 32, 64], initializer=tf.keras.initializers.he_normal())
-    W4 = tf.get_variable('W4', [2, 2, 64, 128], initializer=tf.keras.initializers.he_normal())
+    #W4 = tf.get_variable('W4', [2, 2, 64, 128], initializer=tf.keras.initializers.he_normal())
     parameters = {'W1': W1,
                   'W2': W2,
                   'W3': W3,
-                  'W4': W4,
+                  #'W4': W4,
                   }
     return parameters
 
@@ -60,7 +58,7 @@ def forward_propagation(X, parameters, is_training, drop_rate):
     W1 = parameters['W1']
     W2 = parameters['W2']
     W3 = parameters['W3']
-    W4 = parameters['W4']
+    #W4 = parameters['W4']
 
     Z1 = tf.nn.conv2d(X, W1, strides=[1,1,1,1], padding='SAME')
     A1 = tf.layers.batch_normalization(Z1, training=is_training)
@@ -78,23 +76,23 @@ def forward_propagation(X, parameters, is_training, drop_rate):
     A3 = tf.layers.batch_normalization(Z3, training=is_training)
     A3 = tf.nn.relu(A3)
 
-    #P3 = tf.nn.max_pool(A3, ksize=[1,2,2,1], strides=[1,2,2,1], padding='SAME')
+    P3 = tf.nn.max_pool(A3, ksize=[1,2,2,1], strides=[1,2,2,1], padding='SAME')
 
-    Z4 = tf.nn.conv2d(A3, W4, strides=[1,1,1,1], padding='SAME')
-    A4 = tf.layers.batch_normalization(Z4, training=is_training)
-    A4 = tf.nn.relu(A4)
+    #Z4 = tf.nn.conv2d(A3, W4, strides=[1,1,1,1], padding='SAME')
+    #A4 = tf.layers.batch_normalization(Z4, training=is_training)
+    #A4 = tf.nn.relu(A4)
 
-    P4 = tf.nn.max_pool(A4, ksize=[1,2,2,1], strides=[1,2,2,1], padding='SAME')
+    #P4 = tf.nn.max_pool(A4, ksize=[1,2,2,1], strides=[1,2,2,1], padding='SAME')
 
-    P4 = tf.layers.flatten(P4)
+    P_out = tf.layers.flatten(P3)
 
     fc_layer_one = tf.keras.layers.Dense(30, activation=None)
-    Z4 = tf.layers.batch_normalization(fc_layer_one(P4), training=is_training)
-    Z4 = tf.nn.relu(Z4)
-    Z4 = tf.nn.dropout(Z4, keep_prob=1-drop_rate)
+    Z5 = tf.layers.batch_normalization(fc_layer_one(P_out), training=is_training)
+    Z5 = tf.nn.relu(Z5)
+    Z5 = tf.nn.dropout(Z5, keep_prob=1-drop_rate)
 
     fc_layer_out = tf.keras.layers.Dense(10, activation=None, use_bias=False)
-    Z_out = fc_layer_out(Z4)
+    Z_out = fc_layer_out(Z5)
     return Z_out
 
 
@@ -129,7 +127,7 @@ def random_mini_batches(X, Y, minibatch_size=64, seed=0):
     return mini_batches
 
 
-def model_cnn(train_x, train_y, valid_x, valid_y, test_x, learning_rate=0.0009, num_epoch=100,
+def model_cnn(train_x, train_y, valid_x, valid_y, learning_rate=0.0009, num_epoch=100,
               minibatch_size=64, regularization=0, dropout=0., print_cost=True, draw=False):
 
     (num_sample, n_h, n_w, n_c) = train_x.shape
@@ -138,7 +136,7 @@ def model_cnn(train_x, train_y, valid_x, valid_y, test_x, learning_rate=0.0009, 
     valid_costs = []
 
     X, Y = create_placeholders(n_h, n_w, n_c, n_y)
-    is_training = tf.placeholder(tf.bool)             # for batch normalization
+    is_training = tf.placeholder(tf.bool, name='training')             # for batch normalization
     parameters = initialize_parameters()
 
 # train
@@ -156,8 +154,8 @@ def model_cnn(train_x, train_y, valid_x, valid_y, test_x, learning_rate=0.0009, 
     correct_prediction = tf.equal(tf.argmax(Z_out, 1), tf.argmax(Y, 1))
     accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
 
-    predict_result = tf.argmax(Z_out, 1)
-
+    predict_result = tf.argmax(Z_out, 1, name='output')
+    saver = tf.train.Saver()
     init = tf.global_variables_initializer()
 
 
@@ -211,6 +209,8 @@ def model_cnn(train_x, train_y, valid_x, valid_y, test_x, learning_rate=0.0009, 
         print('Final Train Acc: ', accuracy_train)
         print('Final Validation Acc:', accuracy.eval({X: valid_x, Y: valid_y, is_training: False}))
 
+        saver.save(sess, 'model_file/model.ckpt')
+        '''
         #output test
         out_result = []
         num_out_test = test_x.shape[0]
@@ -224,20 +224,28 @@ def model_cnn(train_x, train_y, valid_x, valid_y, test_x, learning_rate=0.0009, 
             test_out.write('ImageId,Label\n')
             for i in range(len(out_result)):
                 test_out.write('{0},{1}\n'.format(i+1, out_result[i]))
+        '''
         parameters = sess.run(parameters)
     return parameters
 
-'''
-def predict(X, parameters):
-    x = tf.placeholder(tf.float32, [None, 28, 28, 1])
-    Z3 = forward_propagation(x, parameters)
-    prediction = tf.argmax(Z3, 1)
+
+def predict(model_path, test):
+
     init = tf.global_variables_initializer()
     with tf.Session() as sess:
+
         sess.run(init)
-        result = sess.run(prediction, {x: X})
-    return result
-'''
+        saver = tf.train.import_meta_graph('model_file/model.ckpt.meta')
+        saver.restore(sess, tf.train.latest_checkpoint('model_file/'))
+        graph = tf.get_default_graph()
+        X = graph.get_tensor_by_name('input:0')
+        is_training = graph.get_tensor_by_name('training:0')
+        predict_result = graph.get_tensor_by_name('output:0')
+        result = sess.run(predict_result, {X:test, is_training:False})
+        print(result)
+
+    #return result
+
 
 
 if __name__ == '__main__':
@@ -262,26 +270,28 @@ if __name__ == '__main__':
                 regularizer = float(train_opt[i+1])
             elif train_opt[i] == '-d':
                 dropout_rate = float(train_opt[i+1])
-            elif train_opt[i] == '-te':
-                test_file = train_opt[i+1]
             i += 1
 
         (train_X, valid_X), (train_Y, valid_Y) = modified_data(file_input)
         train_X = train_X/255
         valid_X = valid_X/255
+
+        learned_parameters = model_cnn(train_X, train_Y, valid_X, valid_Y, learning_rate=learning_rate,
+                                       num_epoch=epoch_num, minibatch_size=batch_size, regularization=regularizer,
+                                       dropout=dropout_rate, print_cost=True, draw=True)
+    elif opt == '-te':
+        model_path = argv[2]
+        test_file = argv[3]
         real_test_raw = pd.read_csv(test_file)
         num_sample = real_test_raw.shape[0]
         test_X = real_test_raw.values.reshape([num_sample, 28, 28, 1])
+
         increase_contrast = tf.image.adjust_contrast(test_X, 2.0)
         with tf.Session() as sess:
             test_X = sess.run(increase_contrast)
         test_X = test_X/255
-
-        learned_parameters = model_cnn(train_X, train_Y, valid_X, valid_Y, test_X, learning_rate=learning_rate,
-                                       num_epoch=epoch_num, minibatch_size=batch_size, regularization=regularizer,
-                                       dropout=dropout_rate, print_cost=True, draw=True)
+        predict(model_path, test_X)
     '''
-    elif opt == '-te':
         raw_input = pd.read_csv(file_input)
         num_sample = raw_input.shape[0]
         X = raw_input.values.reshape(num_sample, 28, 28, 1)
